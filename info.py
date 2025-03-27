069d5553c0c8b0270929fa9ebc4a86df8618393a
Shubham1245780 <sg3046916@gmail.com>
2025-03-27 12:07:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YW
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002652712405').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://IMovieme:IMovieme@imovieme.odkj7rs.mongodb.net/?retryWrites=true&w=majority&appName=IMovieme
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://IMovieme:IMovieme@imovieme.odkj7rs.mongodb.net/?retryWrites=true&w=majority&appName=IMovieme")
 DATABASE_NAME = environ.get('DATABASE_NAME', "IMovieme")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
